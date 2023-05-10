cf4787f8b0c266946f1d113d047084214153e97b
Aditya Verma <aditya.verma@capgemini.com>
2023-05-10 17:57:36 +0530

    Add and Love animals

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {useState} from 'react';
+import AnimalShow from './showAnimals';
+import './App.css'
+function randomAnimals(){
+    const animals=['bird','cow','gator','dog','horse','cat'];
+    return animals[Math.floor(Math.random()*animals.length)];
 }
+//console.log(randomAnimals());
 
-export default App;
+export default function App(){   
+    const [animals,setAnimals]=useState([]);   
+    
+    const handleCLick = () =>{
+        setAnimals([...animals,randomAnimals()])    
+       
+    };
+    const renderedAnimals=animals.map((animals,index) => {
+        return <AnimalShow type={animals} key={index} />;
+    }
+
+    );
+    
+    return(
+        <div className='app'>
+           <button onClick={handleCLick}> Add animal</button>
+           <div className='animal-list'>{renderedAnimals}</div>
+        </div>
+
+    );
+}
